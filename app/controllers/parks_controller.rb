9dc57b742cb70996c4abc46435d3b243cba2cf38
Kip Kosek <kipkosek@gmail.com>
2017-06-05 16:31:39 -0400

    Add API Query to Search by Facility

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,27 +1,25 @@
 require 'soda/client'
-
 class ParksController < ApplicationController
 
   def index
-    name = params[:name] || ""
-    address = params[:address] || ""
+    name = params[:name].split.map(&:capitalize).join(' ') unless params[:name].blank?
+    # address = params[:address] || ""
     # parameter_hash = { name: name, address: address}
+    facility = params[:facility].downcase unless params[:facility].blank?
     park_request = ParkQuery.new
-    @parks = park_request.get_park(name)
+    if name
+      @parks = park_request.get_park(name)
+    elsif facility
+      # Rails.logger.info(">>>>>>>>>> #{facility.inspect}")
+      query = ParkQuery.get_facility_name_by_display_name(facility)
+      if query
+        @parks = park_request.get_parks_with_facility(query)
+      end
+    else
+      @parks = []
+    end
   end
 
   def show
-
   end
-
-
-  def client
-    @client ||= SODA::Client.new({ :domain => "data.cityofmadison.com" })
-  end
-
-
-  def get_park(name)
-    results = client.get("x4ks-m4xg", { :$limit => 50, :name => name })
-  end
-
 end
--- app/models/park_query.rb
+++ app/models/park_query.rb
@@ -1,15 +1,34 @@
 class ParkQuery
   attr_reader :client
 
+  FACILITY_NAMES = [
+    {query: 'baseball', display_name: 'baseball'},
+    {query: 'b_cycle_station', display_name: 'bike racks'},
+    {query: 'boat_mooring', display_name: 'boat mooring'},
+    {query: 'boat_mooring', display_name: 'boating'}
+  ]
+
   def initialize
     @client ||= SODA::Client.new({ :domain => "data.cityofmadison.com" })
   end
 
-  def get_park(name)
-    results = client.get("x4ks-m4xg", { :$limit => 50, :name => name })
+  def get_park(name_search)
+    client.get("x4ks-m4xg", { :$limit => 50, "$where" => "name LIKE '%#{name_search}%'" })
   end
 
-  def self.has_feature?(park, feature)
-    park.fetch(feature, false)
+  def get_parks_with_facility(facility)
+    client.get("x4ks-m4xg", { :$limit => 50, "$select" => "*", "$where" => "#{facility}=true" })
+  end
+
+  def self.has_facility?(park, facility)
+    park.fetch(facility, false)
+  end
+
+  def self.get_facility_name_by_query(value)
+    FACILITY_NAMES.map {|hash| hash[:display_name] if hash[:query] == value }.compact.first
+  end
+
+  def self.get_facility_name_by_display_name(value)
+    FACILITY_NAMES.map {|hash| hash[:query] if hash[:display_name] == value }.compact.first
   end
 end
